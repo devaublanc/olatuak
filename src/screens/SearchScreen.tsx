c663527bde4e19c25dabf42cab8c8e83111c8c36
Benjamin <benjamin.devaublanc@gmail.com>
2023-04-26 17:33:08 +0200

    remove search screen components

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-import { View } from 'react-native';
-
-import { useTide } from '../hooks/useTide';
-import { TideChart } from '../ui/TideChart/TideChart';
-
 export function SearchScreen() {
-  const { data } = useTide();
-
-  return <View>{data?.length && <TideChart tides={data} />}</View>;
+  return null;
 }
