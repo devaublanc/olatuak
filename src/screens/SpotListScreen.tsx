58d49de9d09d5005c7c277439b4ed80e8a295ce8
Benjamin <benjamin.devaublanc@gmail.com>
2023-05-11 15:58:18 +0200

    remove debug

--- src/screens/SpotListScreen.tsx
+++ src/screens/SpotListScreen.tsx
@@ -1,9 +1,6 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import Constants from 'expo-constants';
 import { useCallback } from 'react';
-import { Text } from 'react-native';
 
-import { Env } from '../config/env';
 import { spots } from '../config/spot';
 import { SpotStackParamList } from '../navigation/RootStack';
 import { ReportPreview } from '../ui/ReportPreview/ReportPreview';
@@ -15,10 +12,8 @@ export function SpotListScreen({ navigation }: SpotListScreenProps) {
   const onPressItem = useCallback((spotId: string) => {
     navigation.navigate('SpotDetail', { spotId });
   }, []);
-  console.log('ENV', Env);
   return (
     <>
-      <Text>APP_VARIANT : {JSON.stringify(Constants.expoConfig?.extra)}</Text>
       <ReportPreview />
       <SpotGrid
         onPressItem={onPressItem}
